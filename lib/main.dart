1fdc6fd833ec9e360c07df18732834b70308bbea
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2025-01-23 17:22:44 -0500

    starting the web dev

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:ofg_web/utils/responsive.dart';
-
+// import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    // options: DefaultFirebaseOptions.currentPlatform,
+    
+);
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCFt1UgnyET1sUemsXPyUjST7MquBZCJzU",
+//   authDomain: "orderflow-general.firebaseapp.com",
+//   projectId: "orderflow-general",
+//   storageBucket: "orderflow-general.appspot.com",
+//   messagingSenderId: "572995758259",
+//   appId: "1:572995758259:web:43304500273b29d0e628c6"
+// };
+
   MobileAds.instance.initialize();
   await SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitUp],
