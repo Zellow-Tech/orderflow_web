93c057706954029bce472aeef1503403001ba035
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2025-01-30 20:56:05 -0500

    endpoints and routes, and middleware for app component

---
+++ lib/routes/app/app_endpoints.dart
@@ -0,0 +1,13 @@
+class OFGEndpoints {
+  static const login = '/login';
+  static const register = '/register';
+
+  static const forgotPassword = '/forgotPassword';
+  static const resetPassword = '/resetPassword';
+
+  static const dashboard = '/dashboard';
+  static const account = '/account';
+  static const settings = '/settings';
+
+  static const unknown = '/404';
+}
