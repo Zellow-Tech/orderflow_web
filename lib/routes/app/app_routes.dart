dc6acc7fbb5c4d92eaa271d26ec5a4064b60b13f
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2025-02-04 15:13:31 -0500

    app route added

--- lib/routes/app/app_routes.dart
+++ lib/routes/app/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:ofg_web/routes/app/app_endpoints.dart';
 import 'package:ofg_web/views/auth/email_verification_page.dart';
 import 'package:ofg_web/views/auth/forgot_password.dart';
@@ -9,21 +10,29 @@ import 'package:ofg_web/views/auth/registration_screen.dart';
 class OFGPages {
   static final pages = [
     GetPage(name: OFGEndpoints.login, page: () => const LoginPage()),
-    GetPage(
-        name: OFGEndpoints.register, page: () => const RegistrationPage()),
+    GetPage(name: OFGEndpoints.register, page: () => const RegistrationPage()),
     GetPage(
         name: OFGEndpoints.forgotPassword,
         page: () => const ForgotPasswordPage()),
+
+    // Pass VendorModel via Get.arguments
     GetPage(
-        name: OFGEndpoints.emailVerifcation,
-        page: () =>  EmailVerificationPage()),
-    // GetPage(name: OFGEndpoints.forgotPassword, page: () => const ForgotPasswordScreen()),
-    // GetPage(name: OFGEndpoints.resetPassword, page: () => const ResetPasswordScreen()),
-    // GetPage(name: OFGEndpoints.dashboard, page: () => const DashboardScreen()),
-    // GetPage(name: OFGEndpoints.account, page: () => const AccountScreen()),
-    // GetPage(name: OFGEndpoints.settings, page: () => const SettingsScreen()),
+      name: OFGEndpoints.emailVerifcation,
+      page: () {
+        final args = Get.arguments;
+        if (args is Map<String, dynamic> && args.containsKey('vendor')) {
+          return EmailVerificationPage(
+            vendor: args['vendor'],
+          );
+        }
+        return Scaffold(body: Center(child: Text('Invalid Arguments')));
+      },
+    ),
+
+    // 
     GetPage(
-        name: OFGEndpoints.unknown,
-        page: () => Scaffold(body: Center(child: Text('Page Not Found'))))
+      name: OFGEndpoints.unknown,
+      page: () => Scaffold(body: Center(child: Text('Page Not Found'))),
+    ),
   ];
 }
