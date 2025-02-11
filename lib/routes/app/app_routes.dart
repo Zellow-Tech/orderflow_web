0d4913ac5ec343e6d87302c8516853f2d21269b8
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2025-02-11 14:10:34 -0500

    dashboard route added to app routes

--- lib/routes/app/app_routes.dart
+++ lib/routes/app/app_routes.dart
@@ -6,14 +6,13 @@ import 'package:ofg_web/views/auth/email_verification_page.dart';
 import 'package:ofg_web/views/auth/forgot_password.dart';
 import 'package:ofg_web/views/auth/login_screen.dart';
 import 'package:ofg_web/views/auth/registration_screen.dart';
+import 'package:ofg_web/views/main/dashboard.dart';
 
 class OFGPages {
   static final pages = [
     GetPage(name: OFGEndpoints.login, page: () => const LoginPage()),
     GetPage(name: OFGEndpoints.register, page: () => const RegistrationPage()),
-    GetPage(
-        name: OFGEndpoints.forgotPassword,
-        page: () => const ForgotPasswordPage()),
+    GetPage(name: OFGEndpoints.forgotPassword, page: () => const ForgotPasswordPage()),
 
     // Pass VendorModel via Get.arguments
     GetPage(
@@ -29,10 +28,10 @@ class OFGPages {
       },
     ),
 
-    // 
-    GetPage(
-      name: OFGEndpoints.unknown,
-      page: () => Scaffold(body: Center(child: Text('Page Not Found'))),
+    GetPage(name: OFGEndpoints.dashboard, page: () => DashboardPage()),
+
+    // for and unknown route
+    GetPage(name: OFGEndpoints.unknown, page: () => Scaffold(body: Center(child: Text('Page Not Found'))),
     ),
   ];
 }
