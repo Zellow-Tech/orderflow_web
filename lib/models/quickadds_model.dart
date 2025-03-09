7ee62c8870a4ad0490c1d67e7823a3d12782d350
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2025-03-09 15:08:14 -0400

    restock checkpoints added, vendor model update for optim

--- lib/models/quickadds_model.dart
+++ lib/models/quickadds_model.dart
@@ -2,20 +2,18 @@ class QuickAddsModel {
   String itemName;
   double qty;
   double price;
-  bool customPrice;
 
-  QuickAddsModel(
-      {required this.itemName,
-      required this.price,
-      required this.customPrice,
-      required this.qty});
+  QuickAddsModel({
+    required this.itemName,
+    required this.price,
+    required this.qty,
+  });
 
   toJson() {
     return {
       'name': itemName,
       'price': price,
       'qty': qty,
-      'customPrice': customPrice
     };
   }
 }
